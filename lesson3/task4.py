1e6601e2703f55a3a82fb405da982054e3d21dc0
n0t0prots <10messiqatar>
2021-04-22 12:04:10 +0300

    redone

--- lesson3/task4.py
+++ lesson3/task4.py
@@ -1,5 +1,4 @@
 list_of_six = [100, 106, 112, 118, 124, 130, 136, 142, 148, 154, 160, 166, 172, 178, 184, 190, 196]
-for i, number in enumerate(list_of_six):
-   if number <= 150 and (float(number/5) - int(number/5) == 0):
-       print(number)
-   continue
+for i in list_of_six:
+    if i % 5 == 0 and i <= 150:
+        print(i)
