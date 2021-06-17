73cb2c855858dfdc77d108abe9e93fce0f817ba6
n0t0prots <10messiqatar>
2021-06-17 19:10:28 +0300

    HW10 redone

--- lesson10/task_2.py
+++ lesson10/task_2.py
@@ -1,26 +1,10 @@
-#def check_if_palindrome():
-#    your_word = input()
- #   if your_word != your_word[::-1]: # -1 здесь шаг строки: от конца к началу
- #       print("It's not palindrome")
- #   else:
-  #      print("It's palindrome")
-# input()
-
-
-# check_if_palindrome()
 def check_if_palindrome():
-    str = input("Enter the string: ")
-    l = len(str)
-    p = l-1
-    index = 0
-    while index < p:
-        if str[index] == str[p]:
-            index = index + 1
-            p = p-1
-        if index == p or index + 1 == p:
-            print("String is a palindrome")
-        else:
-            print("string is not a palindrome")
-        break
+    your_word = input()
+    if your_word != your_word[::-1]: # -1 здесь шаг строки: от конца к началу
+        print("It's not palindrome")
+    else:
+        print("It's palindrome")
+input()
+
 
 check_if_palindrome()
--- lesson10/task_3.py
+++ lesson10/task_3.py
@@ -1,23 +1,7 @@
-def func(l, n):
-    return l[n:] + l[:n]
-try:
-    s=input('Введите список чисел через пробел: ')
-    l = list(map(int, s.split()))
-    if l == []:
-        print('Пусто')
-    else:
-        a=input('Куда сдвиг?(Влево, вправо) - ')
-        n = int(input('На сколько сдвиг? - '))
-        while len(l)<n:
-            n-=len(l)
-        if a[1:5] == 'прав':
-            print(func(l,-n))
-        elif a[1:4] == 'лев':
-            print(func(l,n))
-        else:
-            print('Вы не указали куда сдвинуть')
-except:
-    print('Что-то не так')
+def shift(s, n):
+    n = -n % len(s)
+    return s[n:] + s[:n]
 
 
-func(l, n)
+tupl = [11, 22, 33, 44, 55, 66, 77, 88, 99]
+print(shift(tupl, -3))
